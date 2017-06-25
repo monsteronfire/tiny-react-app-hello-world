3d6242431d8814318712e57a4e2c018fba683eac
Shaii Ong <monsteronfire@gmail.com>
2017-06-25 13:18:58 +0800

    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports {
   entry: './app/index.js',
@@ -11,5 +12,8 @@ module.exports {
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'app/index.html'
+  })]
 }
